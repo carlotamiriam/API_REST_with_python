d3f3052ec177fb1d88e79ceb465e96b5ef602f26
carlotamiriam <carlota_miriam@hotmail.com>
2023-09-09 16:51:56 +0200

    Comments on the code
    Comments on the code

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #The argument name is used to indicate that this Flask application is the main program.
 
 @app.route('/saludo', methods=['GET'])
-def saludar():
+def saludar(): #Defines the saludar() function, which will be executed when a GET request is made to the path '/greeting'.
     return 'Hola, mundo!'
-if __name__ == '__main__':
+if __name__ == '__main__':  #This is a conditional check that verifies if the script is being executed as the main program.
     app.run(debug=True)
